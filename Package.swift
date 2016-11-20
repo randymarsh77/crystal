703f8ef803558e231fde53eda26b10adf5195468
Matthew Preucil <mattpsbs@gmail.com>
2016-11-19 18:49:34 -0800

    Remove 'www' from Package.swift

--- Package.swift
+++ Package.swift
@@ -3,9 +3,9 @@ import PackageDescription
 let package = Package(
     name: "Crystal",
     dependencies: [
-		.Package(url: "https://www.github.com/randymarsh77/cast", majorVersion: 1),
-		.Package(url: "https://www.github.com/randymarsh77/scope", majorVersion: 1),
-		.Package(url: "https://www.github.com/randymarsh77/sockets", majorVersion: 1),
-		.Package(url: "https://www.github.com/randymarsh77/time", majorVersion: 1),
+		.Package(url: "https://github.com/randymarsh77/cast", majorVersion: 1),
+		.Package(url: "https://github.com/randymarsh77/scope", majorVersion: 1),
+		.Package(url: "https://github.com/randymarsh77/sockets", majorVersion: 1),
+		.Package(url: "https://github.com/randymarsh77/time", majorVersion: 1),
 	]
 )
