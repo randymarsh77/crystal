fff3abc98dea492f8ef9bd5706a08aa39b3c3ba0
Matt Preucil <mattpsbs@gmail.com>
2017-02-05 21:02:26 -0800

    Add Cancellation.

--- Package.swift
+++ Package.swift
@@ -3,6 +3,7 @@ import PackageDescription
 let package = Package(
     name: "Crystal",
     dependencies: [
+		.Package(url: "https://github.com/randymarsh77/cancellation", majorVersion: 1),
 		.Package(url: "https://github.com/randymarsh77/cast", majorVersion: 1),
 		.Package(url: "https://github.com/randymarsh77/scope", majorVersion: 1),
 		.Package(url: "https://github.com/randymarsh77/sockets", majorVersion: 1),
